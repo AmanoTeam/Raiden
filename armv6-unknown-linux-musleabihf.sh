c8e464e8c39f34988f6b670b01d018793c6aa20f
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-02-02 13:16:08 -0300

    Update CMake and Autotools toolchains

--- armv6-unknown-linux-musleabihf.sh
+++ armv6-unknown-linux-musleabihf.sh
@@ -2,7 +2,7 @@
 
 declare extra_configure_flags='--with-arch=armv6 --with-fpu=vfp --with-float=hard'
 
-declare triplet='arm-unknown-linux-musleabihf'
+declare triplet='armv6-unknown-linux-musleabihf'
 
 repository='https://dl-cdn.alpinelinux.org/alpine/'
 release='edge'
--- armv7-unknown-linux-musleabihf.sh
+++ armv7-unknown-linux-musleabihf.sh
@@ -2,7 +2,7 @@
 
 declare extra_configure_flags='--with-arch=armv7-a --with-fpu=vfpv3 --with-float=hard'
 
-declare triplet='armv7l-unknown-linux-musleabihf'
+declare triplet='armv7-unknown-linux-musleabihf'
 
 repository='https://dl-cdn.alpinelinux.org/alpine/'
 release='edge'
